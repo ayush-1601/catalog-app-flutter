695bc5278d0f6bebb1b2d61fb6023635dd404499
ayush-1601 <bansalayush@gmail.com>
2021-10-23 11:39:25 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp
+({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Material (
+        child: Center(
+        child: Container(
+        child: Text("welcome to smelco"),
+        ),
+        ),
+        ),
+    );
+  }
+}
